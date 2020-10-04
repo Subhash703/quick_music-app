dcf68a2eb24ab29753b421bbe6c6290cecbed995
Subhash Chandra <subhashchandra30720@gmail.com>
2020-10-04 22:05:53 +0530

    Initial commit, added basic structure of the page

---
+++ js/app.js
@@ -0,0 +1,64 @@
+const EnglishMusic = [
+    {
+        id: 1,
+        songName :"I Tried So Hard Linkin Park",
+        ratings:4,
+        singer : "Linkin Park",
+        totalPlays:20000,
+        url:'./musics/music1.mp3',
+        description:"linking park, one of the best singers ,In The End - Linkin Park (With) music file uploaded on Aug 21st, 2008 by Liamuzumaki."
+    },
+    {
+        id:2,
+        songName :"I Tried So Hard Linkin Park",
+        ratings:4,
+        singer : "Linkin Park",
+        totalPlays:20000,
+        url:'./musics/music1.mp3',
+        description:"linking park, one of the best singers ,In The End - Linkin Park (With) music file uploaded on Aug 21st, 2008 by Liamuzumaki."
+    },
+    {
+        id:3,
+        songName :"I Tried So Hard Linkin Park",
+        ratings:4,
+        singer : "Linkin Park",
+        totalPlays:20000,
+        url:'./musics/music1.mp3',
+        description:"linking park, one of the best singers ,In The End - Linkin Park (With) music file uploaded on Aug 21st, 2008 by Liamuzumaki."
+    },
+    {
+        id:4,
+        songName :"I Tried So Hard Linkin Park",
+        ratings:4,
+        singer : "Linkin Park",
+        totalPlays:20000,
+        url:'./musics/music1.mp3',
+        description:"linking park, one of the best singers ,In The End - Linkin Park (With) music file uploaded on Aug 21st, 2008 by Liamuzumaki."
+    }
+
+]
+console.log(EnglishMusic);
+
+//Search bar background color on typing something
+const searchInput = document.querySelector('#searchInput');
+searchInput.keyup = () =>{
+
+}
+
+const musicList = document.querySelector('.musci_list');
+
+const addSongToList = ()=>{
+    const musicCard = document.createElement('div');
+    musicCard.classList.add('music_card');
+}
+
+function addToCurrentMusic(){
+    
+}
+
+const playSong = ()=>{
+    
+}
+const pauseSong = ()=>{
+    
+}
