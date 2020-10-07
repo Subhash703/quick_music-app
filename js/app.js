d17cdcffa4a37843f2711739db4a64a8a62cafe3
Subhash Chandra <subhashchandra30720@gmail.com>
2020-10-07 22:07:51 +0530

    Javascript Music Card enable and styling for responsive web app

--- js/app.js
+++ js/app.js
@@ -5,8 +5,10 @@ const EnglishMusic = [
         ratings:4,
         singer : "Linkin Park",
         totalPlays:20000,
-        url:'./musics/music1.mp3',
-        description:"linking park, one of the best singers ,In The End - Linkin Park (With) music file uploaded on Aug 21st, 2008 by Liamuzumaki."
+        url:'./src/Linkin Park - In The End.mp3',
+        imageUrl:'./src/img1.jpg',
+        musicUmageUrl:'./src/ima1.jpg',
+        description:"linking park, one of the best singers ,In The End - Linkin Park"
     },
     {
         id:2,
@@ -14,8 +16,9 @@ const EnglishMusic = [
         ratings:4,
         singer : "Linkin Park",
         totalPlays:20000,
-        url:'./musics/music1.mp3',
-        description:"linking park, one of the best singers ,In The End - Linkin Park (With) music file uploaded on Aug 21st, 2008 by Liamuzumaki."
+        imageUrl:'./src/img1.jpg',
+        musicUrl:'./src/Linkin Park - In The End.mp3',
+        description:"linking park, one of the best singers ,In The End - Linkin Park"
     },
     {
         id:3,
@@ -23,8 +26,9 @@ const EnglishMusic = [
         ratings:4,
         singer : "Linkin Park",
         totalPlays:20000,
-        url:'./musics/music1.mp3',
-        description:"linking park, one of the best singers ,In The End - Linkin Park (With) music file uploaded on Aug 21st, 2008 by Liamuzumaki."
+        imageUrl:'./src/img1.jpg',
+        musicUrl:'./src/Linkin Park - In The End.mp3',
+        description:"linking park, one of the best singers ,In The End - Linkin Park"
     },
     {
         id:4,
@@ -32,8 +36,9 @@ const EnglishMusic = [
         ratings:4,
         singer : "Linkin Park",
         totalPlays:20000,
-        url:'./musics/music1.mp3',
-        description:"linking park, one of the best singers ,In The End - Linkin Park (With) music file uploaded on Aug 21st, 2008 by Liamuzumaki."
+        imageUrl:'./src/img1.jpg',
+        musicUrl:'./src/Linkin Park - In The End.mp3',
+        description:"linking park, one of the best singers ,In The End - Linkin Park"
     }
 
 ]
@@ -52,34 +57,85 @@ const addSongToList = ()=>{
     musicCard.classList.add('music_card');
 
     const img = document.createElement('img');
-    img.src = EnglishMusic[current].url;
+    img.src = EnglishMusic[current].imageUrl;
     
     img.setAttribute("alt",`${EnglishMusic[current].songName}`);
     musicCard.appendChild(img);
     
-    const musicDetail = document.createElement('div')
-        .classList.add(music_details);
+    const musicDetail = document.createElement('div');
+    musicDetail.classList.add('music_details');
     
-    const heading = document.createElement('h3').classList.add('music_heading');
+    const heading = document.createElement('h3');
+    heading.classList.add('music_heading');
     heading.innerText = EnglishMusic[current].songName;
     
     musicDetail.appendChild(heading);
 
-    const achivements = document.createElement('span').classList.add(music_achivements);
+    const achivements = document.createElement('span');
+    achivements.classList.add('music_achivements');
 
-    const ratings = document.createElement('span').classList.add('ratings');
+    const ratings = document.createElement('span');
+    ratings.classList.add('ratings');
+    let a = "%";
     
-    ratings.innerText = `"&#9733".${repeat(EnglishMusic[current].ratings)} + "&#9734".${repeat(5 - EnglishMusic[current].ratings)}`;
+    
+    ratings.innerHTML = `${'&#9733'.repeat(EnglishMusic[current].ratings)} + ${"&#9734".repeat((5 - EnglishMusic[current].ratings))}`;
+
+    
     
     achivements.appendChild(ratings);
 
-    const singerName = document.createElement('span').classList.add('singer_name');
+    const singerName = document.createElement('span');
+    singerName.classList.add('singer_name');
     
-    singerName.innerText = `${EnglishMusic[current].singerName}`;
-    singerName.innerText = achivements.appendChild(singerName);
+    singerName.innerHTML = `<i class="fas fa-user"></i> ${EnglishMusic[current].singer}`;
+    achivements.appendChild(singerName);
 
+    const publishedYear = document.createElement('span');
+    publishedYear.classList.add('published_year');
+    publishedYear.innerText = `${Math.floor(Math.random()*10)} year ago`;
+    achivements.appendChild(publishedYear);
+
+    const playedTimes = document.createElement('span');
+    playedTimes.classList.add('played-times');
+    playedTimes.innerHTML = `<i class="fas fa-play"></i>83.4k</span>`;
+    achivements.appendChild(playedTimes);
+
+    musicDetail.appendChild(achivements);
+
+    const music_description = document.createElement('p');
+    music_description.classList.add('music_description');
+    music_description.innerText = `${EnglishMusic[current].description}`;
+    musicDetail.appendChild(music_description);
+
+    const ctrlBtns = document.createElement('div');
+    ctrlBtns.classList.add('control_btns');
+
+    ctrlBtns.innerHTML = `
+                            <span id="playBtn" onClick="javascript:playSong()">
+                                Play
+                            </span>
+                            <span id="pauseBtn" onClick="javascript:pauseSong()">
+                                Pause
+                            </span>
+                            <span class="share">
+                                <i class="fas fa-share"></i>
+                            </span>
+    `
+    musicDetail.appendChild(ctrlBtns);
+    musicCard.appendChild(musicDetail);
+    musicList.appendChild(musicCard);
+    console.log(musicCard);
 }
 
+addSongToList();
+addSongToList();
+addSongToList();
+addSongToList();
+addSongToList();
+addSongToList();
+addSongToList();
+addSongToList();
 function addToCurrentMusic(){
     
 }
